f4740e2c4335feb2d42930b15a815030c29a3e21
gagan.bajaj <gagan.bajaj@xebiaglobal.onmicrosoft.com>
2019-07-04 04:30:25 +0530

    initial commit

---
+++ Olivia-Healthcare-Bot/Dialogs/MainDialog.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Olivia_Healthcare_Bot.Bots.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Olivia_Healthcare_Bot.Dialogs
+{
+    public class MainDialog: ComponentDialog
+    {
+        #region Variables
+        private readonly BotStateService _botStateService;
+
+        //private readonly BotServices _botServices;
+        #endregion
+
+        public MainDialog(BotStateService botStateService) : base(nameof(MainDialog))
+        {
+            _botStateService = botStateService ?? throw new System.ArgumentNullException(nameof(botStateService));
+            //_botServices = botServices ?? throw new System.ArgumentNullException(nameof(botServices));
+
+            InitializeWaterfallDialog();
+        }
+        private void InitializeWaterfallDialog()
+        {
+            // Create Waterfall Steps
+            var waterfallSteps = new WaterfallStep[]
+            {
+                InitialStepAsync,
+                FinalStepAsync
+            };
+
+            // Add Named Dialogs
+            AddDialog(new GreetingDialog($"{nameof(MainDialog)}.greeting", _botStateService));
+            AddDialog(new SymptomCheckerDialog($"{nameof(MainDialog)}.symptomChecker", _botStateService));            
+            AddDialog(new WaterfallDialog($"{nameof(MainDialog)}.mainFlow", waterfallSteps));
+
+            // Set the starting Dialog
+            InitialDialogId = $"{nameof(MainDialog)}.mainFlow";
+        }
+
+        private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+            if (Regex.Match(stepContext.Context.Activity.Text.ToLower(), "hi").Success)
+            {
+                return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.greeting", null, cancellationToken);
+            }
+            else
+            {
+                return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.symptomChecker", null, cancellationToken);
+            }
+
+            // First, we use the dispatch model to determine which cognitive service (LUIS or QnA) to use.
+            //var recognizerResult = await _botServices.Dispatch.RecognizeAsync(stepContext.Context, cancellationToken);
+
+            // Top intent tell us which cognitive service to use.
+            //var topIntent = recognizerResult.GetTopScoringIntent();
+
+            //switch (topIntent.intent)
+            //{
+            //    case "GreetingIntent":
+            //        return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.greeting", null, cancellationToken);
+            //    case "NewBugReportIntent":
+            //        return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.bugReport", null, cancellationToken);
+            //    case "QueryBugTypeIntent":
+            //        return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.bugType", null, cancellationToken);
+            //    default:
+            //        await stepContext.Context.SendActivityAsync(MessageFactory.Text($"I'm sorry I don't know what you mean."), cancellationToken);
+            //        break;
+            //}
+
+            //return await stepContext.NextAsync(null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+
+    }
+}
---
+++ Olivia-Healthcare-Bot/Dialogs/SymptomCheckerDialog.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Olivia_Healthcare_Bot.Bots.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Olivia_Healthcare_Bot.Dialogs
+{
+    public class SymptomCheckerDialog : ComponentDialog
+    {
+        #region Variables
+        private readonly BotStateService _botStateService;
+        #endregion  
+
+        public SymptomCheckerDialog(string dialogId, BotStateService botStateService) : base(dialogId)
+        {
+            _botStateService = botStateService ?? throw new System.ArgumentNullException(nameof(botStateService));
+
+            InitializeWaterfallDialog();
+        }
+
+        private void InitializeWaterfallDialog()
+        {
+            var waterfallSteps = new WaterfallStep[]
+           {
+                BasicSymptomsStepAsync,
+                DurationStepAsync,
+                ActionResultedToTheSymtomsStepAsync,
+                StandardHealthParametersStepAsync
+                //AppointmentStepAsync
+           };
+
+            // Add Named Dialogs
+            AddDialog(new WaterfallDialog($"{nameof(SymptomCheckerDialog)}.mainFlow", waterfallSteps));
+            AddDialog(new TextPrompt($"{nameof(SymptomCheckerDialog)}.BasicSymptoms"));
+            AddDialog(new TextPrompt($"{nameof(SymptomCheckerDialog)}.Duration"));
+            AddDialog(new TextPrompt($"{nameof(SymptomCheckerDialog)}.ActionResultedToTheSymtoms"));
+            AddDialog(new TextPrompt($"{nameof(SymptomCheckerDialog)}.StandardHealthParameters"));
+
+            // Set the starting Dialog
+            InitialDialogId = $"{nameof(SymptomCheckerDialog)}.mainFlow";
+        }
+
+        #region Waterfall Steps
+        private async Task<DialogTurnResult> BasicSymptomsStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync($"{nameof(SymptomCheckerDialog)}.BasicSymptoms",
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Sorry to hear that. What symptoms are bothering you?")                    
+                }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> DurationStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["basicSymptoms"] = (string)stepContext.Result;
+
+            return await stepContext.PromptAsync($"{nameof(SymptomCheckerDialog)}.Duration",
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text($"How long (in hours) has this current {(string)stepContext.Values["basicSymptoms"]} been going on?"),
+                    RetryPrompt = MessageFactory.Text("The value entered must be numeric."),
+                }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActionResultedToTheSymtomsStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["duration"] = (string)stepContext.Result;
+
+            return await stepContext.PromptAsync($"{nameof(SymptomCheckerDialog)}.ActionResultedToTheSymtoms",
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Did you had coffee, energy drink or some sort of soda in last few hours"),
+                    Choices = ChoiceFactory.ToChoices(new List<string> { "Yes", "No" }),
+                }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> StandardHealthParametersStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["ActionResultedToTheSymtoms"] = (string)stepContext.Result;
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("Based on your registered FITBIT data, pulse and heart rate for last hours is fine. Based on your condtion will recomend you to have (PCM) Paracetamol"), cancellationToken);
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+
+        
+        #endregion
+    }
+}
